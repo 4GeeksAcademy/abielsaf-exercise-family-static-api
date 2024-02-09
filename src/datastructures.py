cdae1d45526976ab3d2c483f9604b2c3469165f6
Ángel Bielsa <145146806+abielsaf@users.noreply.github.com>
2024-02-09 19:08:51 +0000

    Modified dataStructures.py

--- src/datastructures.py
+++ src/datastructures.py
@@ -16,21 +16,21 @@ class FamilyStructure:
         self._members = [{
             "id": self._generateId(),
             "first_name": "John",
-            "last_name": "Jackson",
+            "last_name": self.last_name,
             "age": 33,
             "lucky_numbers": [7, 13, 22]
         },
         {
             "id": self._generateId(),
             "first_name": "Jane",
-            "last_name": "Jackson",
+            "last_name": self.last_name,
             "age": 35,
             "lucky_numbers": [10, 14, 3]
         },
         {
             "id": self._generateId(),
             "first_name": "Jimmy",
-            "last_name": "Jackson",
+            "last_name": self.last_name,
             "age": 5,
             "lucky_numbers": [1]
             }
